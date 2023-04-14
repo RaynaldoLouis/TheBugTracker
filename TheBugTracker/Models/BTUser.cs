0ce3294d20fc6a38aca2daf0c170f43b776807d3
kingr <kingr@DESKTOP-5OM5T48>
2023-04-14 16:30:00 -0400

    Add project files.

---
+++ TheBugTracker/Models/BTUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public String LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public String FullName { get { return $"{FirstName}{LastName}"; } }    
+    }
+}
